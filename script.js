6f3c222a0b76f71c21ac65fb833d01bbe5dff2eb
Dodokjr <ffikri604@gmail.com>
2024-02-16 20:06:44 +0700

    jkjk

--- script.js
+++ script.js
@@ -40,3 +40,23 @@ function lightmode()
     localStorage.setItem("dark-mode", "Off");
     document.documentElement.classList.remove("dark");
 }
+
+
+// api call
+
+async function IntegrasiApi()
+{
+    const reslout = document.getElementById("reslout")
+    const type = document.getElementById("type")
+    const srcee = document.getElementById("scree")
+
+    // Api Call
+    let base = "https://www.boredapi.com/api/activity";
+    const req = await fetch(base)
+    const items = await req.json()
+
+    // Api Res
+    reslout.textContent = items.activity
+    type.textContent = `# ${items.type}`
+    type.style.display = "block"
+}
